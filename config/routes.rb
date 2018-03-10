6687cebf66266d05b4156bdf7b7a48c0215ba9cf
Meaghan Lewis <meaghanlewis@github.com>
2018-03-09 22:15:10 -0800

    add button page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pages#home'
   get 'autocomplete', to: 'pages#autocomplete'
+  get 'buttons', to: 'pages#buttons'
   get 'checkbox', to: 'pages#checkbox'
   get 'datepicker', to: 'pages#datepicker'
   get 'dragdrop', to: 'pages#dragdrop'
